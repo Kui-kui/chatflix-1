0c8c906440fa3c8cf87914e5a1853e08c9d09c3d
Cédric Kui <cedrickui@gmail.com>
2017-06-15 19:52:02 +0200

    feat: Ask new question :)

--- recommendation.py
+++ recommendation.py
@@ -74,7 +74,7 @@ class Recommendation:
 
     # Pose une question à l'utilisateur
     def ask_question(self, user):
-        return "Vous aimez les films ?"
+        return "Hello Nikui, quel film voudrais-tu voir aujourd'hui ?"
 
     # Calcule la similarité entre 2 utilisateurs
     @staticmethod
